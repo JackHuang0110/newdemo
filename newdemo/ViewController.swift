e7a6d4634186d21b022f801fa4e162bbaa6aaa6f
Jack Huang <jackhuang@Jackde-MacBook-Air.local>
2018-08-16 12:05:06 +0800

    add new speaker demo

--- newdemo/ViewController.swift
+++ newdemo/ViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //音調＆速度Label顯示到小數點第二位
+    @IBAction func changeSlider(_ sender: Any) {
+        speedRateText.text = String(format: "%.2f", speedRate.value)
+        voiceTypeText.text = String(format: "%.2f", voiceType.value)
+    }
+    
+    //outlet專區
+    @IBOutlet weak var speedRateText: UILabel!
+    @IBOutlet weak var voiceType: UISlider!
+    @IBOutlet weak var voiceTypeText: UILabel!
+    @IBOutlet weak var speedRate: UISlider!
+    @IBOutlet weak var loveTextField: UITextField!
+    
+    //說中文加入音調＆速度功能
+    @IBAction func buttunPressed(_ sender: Any) {
+        
+        let speechUtterance = AVSpeechUtterance(string: loveTextField.text!)
+        let synthesizer = AVSpeechSynthesizer()
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        speechUtterance.pitchMultiplier = voiceType.value
+        speechUtterance.rate = speedRate.value
+        synthesizer.speak(speechUtterance)
 
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
